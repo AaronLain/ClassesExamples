9df83b3fb9451cc1be3ed7697be557324288807a
Aaron Lain <aaron.j.lain@gmail.com>
2020-08-03 20:37:44 -0500

    Add project files.

---
+++ ClassesExamples/Animals/Chicken.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExamples.Animals
+{
+    class Chicken
+    {
+        //property -- order is idiomatic C#
+        public string Name { get; set; }
+
+        //field -- naming convention has _ then camel case 
+        private bool _isHungry = true;
+
+        //constructor
+        public Chicken(string name)
+        {
+            Name = name;
+            Console.WriteLine($"{name} is instantiated!");
+        }
+
+        //method
+        public void Peck(string typeOfFood)
+        {
+            
+            if (_isHungry)
+            {
+                Console.WriteLine($"{Name} pecked hungrily at the {typeOfFood}");
+                _isHungry = false;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is full!");
+            }
+        }
+    }
+}
---
+++ ClassesExamples/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClassesExamples.Animals;
+using System;
+
+
+namespace ClassesExamples // encapsulates an 'idea' of code. Follows folder structure hierarchy. Top of the hierarchy.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chicken = new Chicken("Buttpecker");
+            var bob = new Chicken("Bob");
+
+            // _isHungry is true
+            chicken.Peck("butts"); 
+            bob.Peck("worms");
+
+            // _isHungry is now false
+            chicken.Peck("butts"); 
+            bob.Peck("worms");
+
+        }
+    }
+}
